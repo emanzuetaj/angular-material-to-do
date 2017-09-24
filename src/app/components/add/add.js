6f0c54dbc2b5e8c40a244f84c682486f51eb80d8
Elias J Manzueta <eliasmanz@gmail.com>
2017-09-24 14:11:25 -0400

    Start setting up routing navigation

---
+++ src/app/components/add/add.js
@@ -0,0 +1,31 @@
+(function(angular) {
+    'use strict';
+    /**
+     * @ngdoc component
+     * @name app.component:add
+     *
+     * @description
+     * Add component of the application
+     */
+    angular
+        .module('app')
+        .component('add', {
+            controllerAs: 'vm',
+            templateUrl: 'add/add.html',
+            controller: [AddController]
+        });
+    angular
+        .module('app')
+        .config(['$routeProvider', function($routeProvider) {
+            $routeProvider.when('/add', {
+                name: 'add',
+                component: 'add',
+                url: '/add',
+                template: '<add></add>'
+            });
+        }]);
+    function AddController() {
+        var vm = this;
+        vm.message = 'Add component';
+    }
+})(window.angular);
