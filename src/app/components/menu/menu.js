ca42073d1d5e38143ec83c0e629832f61e786fe5
Elias J Manzueta <eliasmanz@gmail.com>
2017-09-24 22:07:13 -0400

    Set up sidenav to have option selected and get options from controller

--- src/app/components/menu/menu.js
+++ src/app/components/menu/menu.js
@@ -12,11 +12,32 @@
         .component('menu', {
             controllerAs: 'vm',
             templateUrl: 'menu/menu.html',
-            controller: ['$rootScope', '$mdSidenav', MenuController]
+            controller: ['$rootScope', '$mdSidenav', '$location', MenuController]
         });
-    function MenuController($rootScope, $mdSidenav) {
+    function MenuController($rootScope, $mdSidenav, $location) {
+        var vm = this;
+        alert($location.path());
+        vm.options = [
+            {
+                'name': 'Main',
+                'url': './#!/'
+            },
+            {
+                'name': 'Add',
+                'url': './#!/add'
+            }
+        ];
         $rootScope.toggleMenu = function() {
             $mdSidenav('left').toggle();
         };
+        vm.selectOption = function(option) {
+            vm.selected = option;
+        };
+        for (var i = 0; i < vm.options.length; i++) {
+            if (vm.options[i].url.replace('./#!', '') === $location.path()) {
+                vm.selectOption(vm.options[i]);
+                break;
+            }
+        }
     }
 })(window.angular);
