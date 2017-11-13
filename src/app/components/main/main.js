e478a64a4fa227ee3e9ff58576446580690cdffc
Elias J Manzueta <eliasmanz@gmail.com>
2017-11-12 22:43:55 -0500

    Convert data to readable format

--- src/app/components/main/main.js
+++ src/app/components/main/main.js
@@ -27,5 +27,10 @@
     function MainController(toDoService) {
         var vm = this;
         vm.list = toDoService.getList();
+        var date = '';
+        for ( var i = 0; i < vm.list.length; i++) {
+            date = new Date(vm.list[i].date).toLocaleString();
+            vm.list[i].date = date;
+        }
     }
 })(window.angular);
